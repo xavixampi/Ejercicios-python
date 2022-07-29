8b52311964cedf4da02f9caaa6a81548325bae68
xavixampi <77898068+xavixampi@users.noreply.github.com>
2022-07-29 22:14:52 +0200

    Add files via upload

---
+++ Dia1.py
@@ -0,0 +1,5 @@
+#Pide al usuario el valor del radio. A partir de este dato, calcula el área del círculo y muéstrala al usuario
+
+radio=float(input('Introduce el valor del radio:'))
+area=3.1416*(radio**2)
+print(area)
