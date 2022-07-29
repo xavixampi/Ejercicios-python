13703797d4e492f215942574b2b0e23044a94257
xavixampi <77898068+xavixampi@users.noreply.github.com>
2022-07-29 22:14:01 +0200

    Delete 1: Calcular el área de un circulo.py

--- 1: Calcular el área de un circulo.py
+++
@@ -1,5 +0,0 @@
-#Pide al usuario el valor del radio. A partir de este dato, calcula el área del círculo y muéstrala al usuario
-
-radio=float(input('Introduce el valor del radio:'))
-area=3.1416*(radio**2)
-print(area)
