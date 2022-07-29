98a98f3a38d90ca5e9307ffa87e721f44451aab0
xavixampi <77898068+xavixampi@users.noreply.github.com>
2022-07-29 22:29:16 +0200

    Update Dia1.py

--- Dia1.py
+++ Dia1.py
@@ -1,5 +1,12 @@
 #Pide al usuario el valor del radio. A partir de este dato, calcula el área del círculo y muéstrala al usuario
 
+
+#SOLUCIÓN EXPLICADA:
+from math import pi
+
+#1. Pedimos al usuario el valor del radio con un input de tipo float, para que el usuario pueda introducir únicamente números(enteros o decimales).
 radio=float(input('Introduce el valor del radio:'))
-area=3.1416*(radio**2)
+#2.Usamos la formula para hallar el valor del área. la formula es pi*radio**2
+area=pi*(radio**2)
+#Mostramos por pantalla el valor del área
 print(area)
