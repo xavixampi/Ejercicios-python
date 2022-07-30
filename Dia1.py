ad6dfaf4df889b7969da6820c3e0d32fda4e9530
xavixampi <77898068+xavixampi@users.noreply.github.com>
2022-07-30 15:52:15 +0200

    Update Dia1.py

--- Dia1.py
+++ Dia1.py
@@ -6,7 +6,7 @@ from math import pi
 
 #1. Pedimos al usuario el valor del radio con un input de tipo float, para que el usuario pueda introducir únicamente números(enteros o decimales).
 radio=float(input('Introduce el valor del radio:'))
-#2.Usamos la formula para hallar el valor del área. la formula es pi*radio**2
+#2.Usamos la fórmula para hallar el valor del área. La fórmula es pi*radio**2
 area=pi*(radio**2)
 #Mostramos por pantalla el valor del área
 print('El valor del area es:',area)
